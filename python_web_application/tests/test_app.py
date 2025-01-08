76dfeea79da70e8a0fdb838f5a1432f4bc5ff322
Abhijot <kaurab@shernet.sheridancollege.ca>
2025-01-08 00:44:38 -0500

    commit

--- python_web_application/tests/test_app.py
+++ python_web_application/tests/test_app.py
@@ -1,7 +1,5 @@
 # tests/test_app.py
 from python_web_application.python_web_application.app import app_function
 import pytest 
-from python_web_application.python_web_application.app import app_function  # Replace with the function you want to test
-
 def test_app_function():
     assert app_function() == "Hello World"  # Replace with the expected outcome
