3e771919391a69eb45fd261aff836dd1716a1d45
Abhijot <kaurab@shernet.sheridancollege.ca>
2025-01-08 00:00:14 -0500

    changing structure

---
+++ python_web_application/test_app.py
@@ -0,0 +1,7 @@
+# tests/test_app.py
+from python_web_application.python_web_application.app import app_function
+import pytest 
+from python_web_application.python_web_application.app import app_function  # Replace with the function you want to test
+
+def test_app_function():
+    assert app_function() == "Hello World"  # Replace with the expected outcome
--- test_app.py
+++
@@ -1,7 +0,0 @@
-# tests/test_app.py
-from python_web_application.app import app_function
-import pytest 
-from app import app_function  # Replace with the function you want to test
-
-def test_app_function():
-    assert app_function() == "Hello World"  # Replace with the expected outcome
